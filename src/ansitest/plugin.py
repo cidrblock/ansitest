a0a4209b674628f42dfea5cb183584185528051a
Kate Case <kcase@redhat.com>
2023-04-25 09:45:02 -0400

    Remove 2.10 and 2.11 runs

--- src/ansitest/plugin.py
+++ src/ansitest/plugin.py
@@ -36,8 +36,8 @@ ALLOWED_EXTERNALS = [
     "echo",
 ]
 ENV_LIST = """
-{integration, sanity, unit}-py3.8-{2.9, 2.10, 2.11, 2.12, 2.13}
-{integration, sanity, unit}-py3.9-{2.10, 2.11, 2.12, 2.13, 2.14, milestone, devel}
+{integration, sanity, unit}-py3.8-{2.9, 2.12, 2.13}
+{integration, sanity, unit}-py3.9-{2.12, 2.13, 2.14, milestone, devel}
 {integration, sanity, unit}-py3.10-{2.12, 2.13, 2.14, milestone, devel}
 {integration, sanity, unit}-py3.11-{2.14, milestone, devel}
 """
